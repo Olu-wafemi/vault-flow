528f79a3b8a158ae838c55efb3cf14fb284d1ca5
Oluwafemi <femio82@gmail.com>
2025-02-18 16:00:26 +0100

    feat: create microservice for transactions

--- src/transaction/transaction.main.ts
+++ src/transaction/transaction.main.ts
@@ -1,3 +1,19 @@
-import { MessagePattern } from "@nestjs/microservices";
+import { NestFactory } from "@nestjs/core";
+import { MessagePattern, MicroserviceOptions, Transport } from "@nestjs/microservices";
+import { TransactionModule } from './transaction.module';
 
-@MessagePattern
+
+async function bootstrap(){
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(TransactionModule,{
+
+        transport: Transport.TCP,
+        options:{
+            host: "0.0.0.0",
+            port: 30002,
+        }
+
+    })
+
+    await app.listen()
+}
+bootstrap()
