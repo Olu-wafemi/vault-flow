6a7f7a77f264edcaabdc77a1cf28a542ec027f56
Oluwafemi <femio82@gmail.com>
2025-02-18 16:35:32 +0100

    feat: microservice transaction controller

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,4 +1,18 @@
 import { Controller } from '@nestjs/common';
+import { TransactionService } from './transaction.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('transaction')
-export class TransactionController {}
+export class TransactionController {
+    constructor(private readonly transactionService: TransactionService){}
+
+
+    @MessagePattern({ cmd: "transferFunds"})
+    async transferFunds(
+        @Payload() data: {fromWalletId: string, toWalletId: string, amount: number, idempotencyKey: string}
+    ){
+        return await this.transactionService.transferfunds(
+            data.toWalletId, data.toWalletId, data.amount,data.idempotencyKey
+        )
+    }
+}
