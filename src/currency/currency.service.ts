d83216b0070e08fc2262c14affcdb92fdcc90a31
Oluwafemi <femio82@gmail.com>
2025-02-18 14:59:33 +0100

    try, catch block

--- src/currency/currency.service.ts
+++ src/currency/currency.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, HttpException, HttpStatus } from '@nestjs/common';
 import { HttpService } from "@nestjs/axios"
 import {CACHE_MANAGER, Cache} from "@nestjs/cache-manager"
 import { lastValueFrom } from 'rxjs';
@@ -17,7 +17,7 @@ export class CurrencyService {
 
     async getExchangerates(){
         let getrates: any
-
+        try{
         const checkrates = this.cacheManager.get("rates")
         if(!checkrates){
             getrates = this.httpService.get("https://api.fastforex.io/fetch-all?api_key=7df3b391e3-7a16ec8337-srvrnn")
@@ -25,14 +25,24 @@ export class CurrencyService {
             this.cacheManager.set("rates", response)
         }
         return getrates
+    }
+    catch{
+        throw new HttpException ("Failed to fetch exchange rates", HttpStatus.BAD_GATEWAY)
+    }
 
     }
 
     async convertCurrency(from: string, amount: number, to: string){
+        try{
 
         const convert = this.httpService.get(`https://api.fastforex.io/convert?from=${from}&to=${to}&amount=${amount}&api_key=7df3b391e3-7a16ec8337-srvrnn`)
         const response = await  lastValueFrom(convert)
         return response.data.result.to
+        }
+        catch{
+            throw new HttpException ("Failed to fetch exchange rates", HttpStatus.BAD_GATEWAY)
+    
+        }
     }
 
     
