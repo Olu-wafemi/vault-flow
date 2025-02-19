7f5ee69667992ede3d2b9a27749402e8f8fa9abd
Oluwafemi <femio82@gmail.com>
2025-02-19 14:21:12 +0100

    set up fraud detection module

--- src/fraud-detection/fraud-detection.module.ts
+++ src/fraud-detection/fraud-detection.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { FraudDetectionService } from './fraud-detection.service';
+import { FraudDetectionController } from './fraud-detection.controller';
 
-@Module({})
+@Module({
+    providers: [FraudDetectionService],
+    controllers: [FraudDetectionController]
+})
 export class FraudDetectionModule {}
