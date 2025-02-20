f7b4305983379db800d2e035dc9dc1435251f131
Oluwafemi <femio82@gmail.com>
2025-02-20 20:59:42 +0100

    feat: login test

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,14 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
-import { UsersService } from 'src/users/users.service';
+import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from "bcrypt"
+import { User, UserRole } from '../users/user.entity';
+
 
 describe('AuthService', () => {
   let authService: AuthService;
   let usersService: Partial<UsersService>;
   let jwtService: Partial<JwtService>
-
   beforeEach(async () => {
 
     const usersServiceMock = {
@@ -18,13 +19,18 @@ describe('AuthService', () => {
       updateRefreshToken: jest.fn()
 
     }
+
+    const AuthServiceMock = {
+      validateUser: jest.fn()
+    }
     const jwtServiceMock = {
       sign: jest.fn()
     }
     const module: TestingModule = await Test.createTestingModule({
       providers: [AuthService,
         {provide: UsersService, useValue: usersServiceMock},
-        {provide: JwtService, useValue: jwtServiceMock}
+        {provide: JwtService, useValue: jwtServiceMock},
+        
       ],
     }).compile();
 
@@ -35,7 +41,19 @@ describe('AuthService', () => {
 
   });
 
-  it('should be defined', () => {
-    expect(authService).toBeDefined();
-  });
+  describe("login", ()=>{
+    it('should return access and refreshTokens on Login', async()=>{
+
+      const plainPassword = "fascinator"
+      const hashedPassword = await bcrypt.hash(plainPassword, 10)
+
+      const user = {id: '123', username: "effemm", password: hashedPassword };
+      const users = {id: '123', username: 'effemm', password: hashedPassword, refreshToken: 'fake' , roles: UserRole.ADMIN};
+      (usersService.findUserByUsername as jest.Mock).mockResolvedValue(user);
+      (jwtService.sign as jest.Mock).mockReturnValue('fake');
+       jest.spyOn(authService, 'validateUser').mockResolvedValue(users);
+       const result = await authService.login({username: "test", password: plainPassword})
+       expect(result).toHaveProperty("accessToken", "fake")
+    })
+  })
 });
