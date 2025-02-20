b4480507a104b090f7948736c4375f822222c6a4
Oluwafemi <femio82@gmail.com>
2025-02-20 19:12:38 +0100

    feat: setup describe Auth tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from "bcrypt"
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let usersService: Partial<UsersService>;
+  let jwtService: Partial<JwtService>
 
   beforeEach(async () => {
+
+    const usersServiceMock = {
+      findUserByUsername: jest.fn(),
+      createUser: jest.fn(),
+      findOneById: jest.fn(),
+      updateRefreshToken: jest.fn()
+
+    }
+    const jwtServiceMock = {
+      sign: jest.fn()
+    }
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService,
+        {provide: UsersService, useValue: usersServiceMock},
+        {provide: JwtService, useValue: jwtServiceMock}
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService)
+    jwtService = module.get<JwtService>(JwtService)
+
+
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
   });
 });
