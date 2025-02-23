b13f67be24fddca4e586a8ad7d7832e688494d42
Oluwafemi <femio82@gmail.com>
2025-02-23 20:08:05 +0100

    test for create user service

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+//import { UseInterceptors } from '@nestjs/common';
+
 
 describe('UsersService', () => {
-  let service: UsersService;
+
+  let usersService: UsersService;
+
+  let usersRepo: Repository<User>;
 
   beforeEach(async () => {
+    
+
+    const usersRepoMock = {
+      create: jest.fn(),
+      findOne: jest.fn(),
+      update: jest.fn(),
+      save: jest.fn()
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      
+      providers: [UsersService,{
+        provide: getRepositoryToken(User),
+        useValue: usersRepoMock
+      }],
     }).compile();
-
-    service = module.get<UsersService>(UsersService);
+    usersService = module.get<UsersService>(UsersService);
+    usersRepo = module.get<Repository<User>>(getRepositoryToken(User))
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+  describe("CreateUser", ()=>{
+    it("Should create a user", async()=>{
+
+      const user = {username: "test", password: "test"};
+
+      
+
+
+      (usersRepo.create as jest.Mock).mockResolvedValue(user);
+      (usersRepo.save as jest.Mock).mockResolvedValue(user);
+
+      const result = await usersService.CreateUser("test", "test");
+
+      expect(result).toHaveProperty("username", "test")
+    
+    })
+  })
 });
