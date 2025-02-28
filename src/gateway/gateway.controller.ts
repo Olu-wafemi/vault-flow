e178a9fe19caf1315887dc923d0f1f4723aa365d
Oluwafemi <femio82@gmail.com>
2025-02-28 13:20:24 +0100

    refactor: api gateway

--- src/gateway/gateway.controller.ts
+++ src/gateway/gateway.controller.ts
@@ -1,34 +1,85 @@
 import {Controller, Post, Get, Body, Query} from "@nestjs/common"
 import { ClientProxy, ClientProxyFactory,Transport } from "@nestjs/microservices"
 import { lastValueFrom } from "rxjs"
+import { Wallet } from 'src/wallet/wallet.entity';
 
-@Controller('wallet')
+@Controller()
 export class GatewayController{
-    private client: ClientProxy
+    private walletClient: ClientProxy;
+    private transactionClient: ClientProxy
     constructor(){
-        this.client = ClientProxyFactory.create({
+        this.walletClient = ClientProxyFactory.create({
             transport: Transport.TCP,
             options:{
                 host: "localhost",
                 port: 3001
             }
         })
+
+        this.transactionClient= ClientProxyFactory.create({
+            transport: Transport.TCP,
+            options:{
+                host: "localhost",
+                port: 3002,
+            }
+        })
     }
 
-    @Post()
+    @Post('wallets')
     async createWallet(@Body() body:{userId: number, currency: string}){
         const pattern = {cmd : 'createWallet'}
         const payload = {userId: body.userId, curency: body.currency};
 
-        const result = await lastValueFrom(this.client.send(pattern, payload));
+        const result = await lastValueFrom(this.walletClient.send(pattern, payload));
         return result
     } 
-    @Get()
+    @Get('wallets')
     async getWallets(@Query("userId") userId: number){
         const pattern = {cmd: "getWallets"}
         const payload = {userId: Number(userId)};
-        const result = await lastValueFrom(this.client.send(pattern, payload))
+        const result = await lastValueFrom(this.walletClient.send(pattern, payload))
         return result;
     }
+    @Post('wallets/deposit')
+    async deposit(@Body() body: {walletId: string; amount: number; idempotencyKey: string}){
+        const pattern = {cmd: 'deposit'}
+        const payload = {
+            walletId: body.walletId,
+            amount: body.amount,
+            idempotencyKey: body.idempotencyKey
+        }
+        const result = await lastValueFrom(this.walletClient.send(pattern, payload))
+        return result;
+    }
+    @Post('wallets/withdraw')
+    async withdrawalsSchema(@Body() body:{walletId: string; amount: number; idempotencyKey: string}){
+        const payload = {
+            walletId: body.walletId,
+            amount: body.amount,
+            idempotencyKey: body.idempotencyKey
+        }
+        const pattern = {cmd: "withdraw"}
+
+        const result = await lastValueFrom(this.walletClient.send(pattern, payload))
+        return result
+    }
+
+    @Post('transaction/transfer')
+    async transfer(
+        @Body() body: {fromWalletId : string; toWalletId: string; amount: number; idempotencyKey: string; }
+    ){
+        const pattern = { cmd: "transfer"}
+        const payload = {
+            fromWalletId: body.fromWalletId,
+            toWalletId: body.toWalletId,
+            amount: body.amount,
+            idempotencyKey: body.idempotencyKey
+        }
+
+        const result = await lastValueFrom(this.transactionClient.send(pattern, payload))
+
+    }
+
+    
 
 }
