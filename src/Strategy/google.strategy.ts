a0dbe5329d2f4cf02c42fe7e3da8cffe71118809
Oluwafemi <femio82@gmail.com>
2025-03-13 21:00:10 +0100

    oauth strategy

--- src/Strategy/google.strategy.ts
+++ src/Strategy/google.strategy.ts
@@ -1,11 +1,19 @@
 import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
+import { Strategy } from 'passport-jwt';
 
 
 
 
 
 @Injectable()
-exports class GoogleStrategy extends PassportStrategy(){
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google'){
+
+    constructor(private config: ConfigService){
+        super({
+
+        })
+    }
 
 }
