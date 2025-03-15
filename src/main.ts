174910f81399847ad36e69bf809b0f3c8a9f6de8
Oluwafemi <femio82@gmail.com>
2025-03-15 15:53:30 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionFilter())
 
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen( process.env.PORT || 8080);
 }
 
 
