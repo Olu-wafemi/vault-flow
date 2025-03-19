2362c0a72011c7003e8187465658768c74472bb3
Oluwafemi <femio82@gmail.com>
2025-03-19 23:57:24 +0100

    finished googleoauth stategy

--- src/Strategy/google.strategy.ts
+++ src/Strategy/google.strategy.ts
@@ -12,8 +12,31 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google'){
 
     constructor(private config: ConfigService){
         super({
+            clientID: config.get('GOOGLE_CLIENT_ID'),
+            clientSecret: config.get('GOOGLE_CLIENT_SECRET'),
+            callbackURL: 'http://localhost:3000/auth/google/callback',
+            scope: ['email', 'profile'],
 
         })
     }
+
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    done,
+  ) {
+    const { name, emails, photos } = profile;
+    const user =  {
+      email: emails[0].value,
+      firstname: name.givenName,
+      lastname: name.familyName,
+      picture: photos[0].value,
+      accessToken,
+    };
+    done(null, user);
+  }
 
 }
