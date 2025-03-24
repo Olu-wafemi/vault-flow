7e25c7223521b4b238ea23805e6d93a5dba77d03
Oluwafemi <femio82@gmail.com>
2025-03-24 23:51:13 +0100

    facebook strategy

--- src/Strategy/facebook.strategy.ts
+++ src/Strategy/facebook.strategy.ts
@@ -3,3 +3,15 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-facebook';
 import { VerifiedCallback } from 'passport-jwt';
+
+@Injectable()
+export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
+  constructor(private config: ConfigService) {
+    super({
+      clientID: config.get('FACEBOOK_APP_ID'),
+      clientSecret: config.get('FACEBOOK_APP_SECRET'),
+      callbackURL: 'http://localhost:3000/auth/facebook/callback',
+      profileFields: ['email', 'name', 'displayName', 'photos'],
+      scope: ["email"]
+    });
+  }
